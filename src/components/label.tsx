9f24f57f3c11e5a96adf436932b38205be8796cc
Matheus <matheuschmidt.b@gmail.com>
2024-07-04 17:17:37 -0300

    style: Update Label component styling for better visibility

--- src/components/label.tsx
+++ src/components/label.tsx
@@ -5,7 +5,7 @@ interface LabelProps {
 const Label = ({ children }: LabelProps) => {
   return (
     <div
-      className={`absolute right-4 top-4 flex size-10 items-center justify-center rounded-full ${children === "New" ? "bg-emerald-500" : "bg-rose-500"} text-xs text-white`}
+      className={`absolute right-4 top-4 flex size-10 items-center justify-center rounded-full shadow-md ${children === "New" ? "bg-emerald-500" : "bg-rose-500"} text-xs text-white`}
     >
       {children}
     </div>
